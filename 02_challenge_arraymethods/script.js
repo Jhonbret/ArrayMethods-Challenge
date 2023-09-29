0bec1510e8f7d2be2fcd2ea65fb61de26160470d
jhonbret <jhonbrettangaha9@gmail.com>
2023-09-29 08:35:08 +0800

    JS SOlution

---
+++ 02_challenge_arraymethods/script.js
@@ -0,0 +1,37 @@
+// my Solution
+
+// Challenge 1
+
+// const people = [
+//   { name: "John Doe", age: 30, email: "john@example.com" },
+//   { name: "Jane Poe", age: 25, email: "john@example.com" },
+//   { name: "Bob Foe", age: 45, email: "john@example.com" },
+//   { name: "Sara Soe", age: 19, email: "jane@example.com" },
+//   { name: "Jose koe", age: 23, email: "bob@example.com" },
+// ];
+
+// const youngPeople = people
+//   .filter((person) => person.age <= 25)
+//   .map((person) => ({ name: person.name, email: person.email }));
+
+// console.log(youngPeople);
+
+// Challenge 2
+
+// const numbers = [5, -2, 12, 8, -7, 30];
+// let sumOfPositives = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 0) {
+//     sumOfPositives += numbers[i];
+//   }
+// }
+// console.log(sumOfPositives);
+
+// Challenge 3
+
+// const words = ["coder", "programmer", "developer"];
+// const capitalizedWords = words.map(
+//   (word) => word.charAt(0).toUpperCase() + word.slice(1)
+// );
+
+// console.log(capitalizedWords);
